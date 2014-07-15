22aeffdd7ff7c3212e1cbf730677e91481dfc73e
s.vandamme <sebastien.vandamme@gmail.com>
2014-07-15 13:54:14 +0200

    Hibernate classes creation

--- src/main/java/com/seb/services/weather/data/orm/City.java
+++ src/main/java/com/seb/services/weather/data/orm/City.java
@@ -3,17 +3,41 @@ package com.seb.services.weather.data.orm;
 import com.seb.services.weather.enums.Province;
 import com.seb.services.weather.enums.Region;
 
+import javax.persistence.*;
+import java.util.List;
+
 /**
  * Created by sebastien.vandamme@gmail.com on 12/07/2014.
  */
+@Entity
 public class City {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
     private String name;
+
     private int population;
+
+    @Enumerated(EnumType.STRING)
     private Province province;
+
+    @Enumerated(EnumType.STRING)
     private Region region;
+
+    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL, mappedBy = "id")
+    @JoinColumn(name = "id")
+    private List<TemperatureHistory> temperatureHistory;
+
+    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL, mappedBy = "id")
+    private List<WeatherHistory> weatherHistory;
 
     public City() {
         super();
+    }
+
+    public int getId() {
+        return id;
     }
 
     public String getName() {
@@ -46,5 +70,21 @@ public class City {
 
     public void setRegion(Region region) {
         this.region = region;
+    }
+
+    public List<TemperatureHistory> getTemperatureHistory() {
+        return temperatureHistory;
+    }
+
+    public void setTemperatureHistory(List<TemperatureHistory> temperatureHistory) {
+        this.temperatureHistory = temperatureHistory;
+    }
+
+    public List<WeatherHistory> getWeatherHistory() {
+        return weatherHistory;
+    }
+
+    public void setWeatherHistory(List<WeatherHistory> weatherHistory) {
+        this.weatherHistory = weatherHistory;
     }
 }
---
+++ src/main/java/com/seb/services/weather/data/orm/TemperatureHistory.java
@@ -0,0 +1,69 @@
+package com.seb.services.weather.data.orm;
+
+import org.hibernate.annotations.Type;
+import org.joda.time.LocalDate;
+
+import javax.persistence.*;
+
+/**
+ * Created by sebastien.vandamme@gmail.com on 15/07/2014.
+ */
+@Entity
+public class TemperatureHistory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id", nullable = false)
+    private City city;
+
+    private int temperature;
+
+    @Type(type = "org.joda.time.contrib.hibernate.PersistentLocalDate")
+    private LocalDate date;
+
+    @Type(type = "org.joda.time.contrib.hibernate.PersistentLocalTimeAsTime")
+    private LocalDate time;
+
+    public TemperatureHistory() {
+        super();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public City getCity() {
+        return city;
+    }
+
+    public void setCity(City city) {
+        this.city = city;
+    }
+
+    public int getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(int temperature) {
+        this.temperature = temperature;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public LocalDate getTime() {
+        return time;
+    }
+
+    public void setTime(LocalDate time) {
+        this.time = time;
+    }
+}
